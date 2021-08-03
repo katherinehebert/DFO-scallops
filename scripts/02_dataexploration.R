6421d17313375aca2e44bf18a94e2f22b9f99936
Katherine Hébert <katherine.ahebert@gmail.com>
2021-08-03 14:47:43 -0400

    make some exploratory plots

--- scripts/02_dataexploration.R
+++ scripts/02_dataexploration.R
@@ -4,6 +4,10 @@ rm(list=ls())
 
 library(ggplot2)
 library(readr)
+library(dplyr)
+library(tidyr)
+library(patchwork)
+library(scales)
 
 theme_set(ggpubr::theme_pubclean())
 
@@ -91,8 +95,64 @@ ggplot(data = cpue_summ,
 # read the prepared data
 sizes <- read_csv("data/scallop-sizes.csv")
 
-# group by fishing area and zone
-cpue_gr <- cpue %>%
-  group_by(Div, Quadril, Zone_ges) 
-
 ## exploratory plots
+
+# stacked barchart of size cateogries through time
+sizes.toplot <- sizes
+sizes.toplot$Year <- as.character(sizes$Year)
+sizes.toplot$Length <- factor(sizes$Length, levels = c(1:120, NA))
+#sizes.toplot$Length[which(sizes$Length %in% 1:9)] <- paste0("0", sizes.toplot$Length[which(sizes$Length %in% 1:10)])
+p <- ggplot(sizes.toplot) +
+  geom_bar(aes(x = Year, 
+               y = nlengthm2, 
+               fill = Length),
+           position = "stack", 
+           stat = "identity",
+           width = .6) +
+  labs(x = "", y = "Density at each length") +
+  facet_wrap(~Sector) +
+  scale_fill_manual(values = hex_codes1) +
+  theme(
+    legend.position = "none",
+        axis.text.x = element_text(angle = 90, size = 6))
+
+
+# print a continuous legend separately
+n1 <- 121                                        # Amount of default colors
+hex_codes1 <- hue_pal()(n1)                             # Identify hex codes
+names(hex_codes1) <- levels(sizes.toplot$Length)
+p0 <- ggplot(sizes) +
+  geom_point(aes(x = Year, 
+                 y = nlengthm2, 
+                 col = Length)) +
+  facet_wrap(~Sector) +
+  scale_color_gradientn(colors = hex_codes1) +
+  theme(legend.position = "top",
+        axis.text.x = element_text(angle = 90, size = 6))
+plotlegend <- cowplot::get_legend(p0)
+p0 <- ggpubr::as_ggplot(plotlegend)
+
+# patchwork the plot and its legend together
+p0 / p + 
+  plot_layout(heights = unit(c(1.5, 1), c('cm', 'null')))
+ggsave("figures/density_lengths_persector.png", width = 8.42, height = 8.6)
+
+
+# same, but percent barchart
+p2 <- ggplot(sizes.toplot) +
+  geom_bar(aes(x = Year, 
+               y = nlengthm2, 
+               fill = Length),
+           position = "fill", 
+           stat = "identity",
+           width = .6) +
+  labs(x = "", y = "Density at each length") +
+  facet_wrap(~Sector) +
+  scale_fill_manual(values = hex_codes1) +
+  theme(
+    legend.position = "none",
+    axis.text.x = element_text(angle = 90, size = 6))
+p0 / p2 + 
+  plot_layout(heights = unit(c(1.5, 1), c('cm', 'null')))
+ggsave("figures/density_lengths_persector_fill.png", width = 8.42, height = 8.6)
+
