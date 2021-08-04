24b1d384611583df293dcfb0e8c24d9a9f2e75fa
Katherine Hébert <katherine.ahebert@gmail.com>
2021-08-04 15:26:08 -0400

    initial commit

---
+++ scripts/model_length_time.R
@@ -0,0 +1,106 @@
+# hierarchical GAM to model length differences through time
+
+# load packages
+library(Hmisc)
+library(mgcv)
+
+# read the prepared data
+sizes <- read_csv("data/scallop-sizes.csv")
+
+# calculate weighted mean of lengths over time, per sector
+# lengths are weighted by the Count of individuals at each length
+meanlengths <- sizes %>%
+  group_by(Sector, Year) %>%
+  # weighted average length via density!
+  summarise(mean_length = wtd.mean(x = Length, weights = Count, 
+                                   na.rm = TRUE, normwt = FALSE),
+            # weighted variance
+            var_length = wtd.var(x = Length, weights = Count, 
+                                 na.rm = TRUE, normwt = FALSE)) %>%
+  mutate(sd_length = sqrt(var_length))
+
+# plot time series of mean lengths per sector  
+ggplot(meanlengths) +
+  geom_ribbon(aes(x = Year,
+                  ymin = mean_length - 2*sd_length,
+                  ymax = mean_length + 2*sd_length,
+                  fill = Sector),
+              alpha = .1) +
+  geom_line(aes(x = Year, y = mean_length, col = Sector)) +
+  theme(legend.position = "none",
+        axis.text.x = element_text(angle = 90)) +
+  labs(x = "", y = "Mean length") +
+  facet_wrap(~Sector)
+ggsave("figures/meanlengths_persector.png", width = 6.28, height = 8.74)
+  
+
+# does this require a linear mixed model? --------------------------------------
+
+lm.test <- lm(mean_length ~ Year, data = meanlengths)
+# get residuals
+lm.test.resid <- resid(lm.test)
+
+# remove NAs
+temp <- meanlengths[-which(is.na(meanlengths$mean_length)),]
+
+plot(lm.test.resid ~ as.factor(temp$Sector), 
+     xlab = "Sector",
+     ylab = "Standardized residuals")
+
+abline(0, 0, lty = 2)
+# there is residual variance that varies by sector, so good idea to try a mixed model
+
+# mixed model(s) compared to basic linear model
+library(lme4)
+m0 <- lm(mean_length ~ Year, 
+         data = meanlengths)
+m1 <- lmer(mean_length ~ Year + 1|Sector, 
+           data = meanlengths)
+m2 <- lmer(mean_length ~ Year|Sector, 
+            data = meanlengths)
+hist(resid(m0)) # gaussian should be fine here, the residuals are pretty Normal
+hist(resid(m1))
+hist(resid(m2))
+
+# compare
+AIC.table <- MuMIn::model.sel(m0, m1, m2)
+# basic linear model still comes through as the best option
+broom::tidy(m0)
+summary(m0)
+
+
+# plot the model over the time series
+ggplot(meanlengths) +
+  geom_line(aes(x = Year, y = mean_length, col = Sector)) +
+  geom_smooth(aes(x = Year, y = mean_length), 
+              method = "lm", col = "black") +
+  theme(legend.position = "right") +
+  labs(x = "", y = "Mean length",
+       caption = paste("Adj R2 = ",signif(summary(m0)$adj.r.squared, 2),
+                     " Slope =",signif(m0$coef[[2]], 5),
+                     " P =",signif(summary(m0)$coef[2,4], 5)))
+ggsave("figures/meanlengths_lm.png", width = 6.83, height = 5.03)
+
+
+# plot the model over boxplots of the mean lengths over all sectors
+# to see the variation better
+ggplot(meanlengths) +
+  geom_boxplot(aes(x = Year, y = mean_length, 
+                   group = factor(Year), fill = mean_length)) +
+  geom_smooth(aes(x = Year, y = mean_length), 
+              method = "lm", col = "black") +
+  theme(legend.position = "right") +
+  labs(x = "", y = "Mean length",
+       caption = paste("Adj R2 = ",signif(summary(m0)$adj.r.squared, 2),
+                       " Slope =",signif(m0$coef[[2]], 5),
+                       " P =",signif(summary(m0)$coef[2,4], 5)))
+ggsave("figures/meanlengths_lm_boxplot.png", width = 6.83, height = 5.03)
+
+
+# plot with a GAM just out of curiosity
+# ggplot(meanlengths) +
+#   geom_line(aes(x = Year, y = mean_length, col = Sector)) +
+#   geom_smooth(aes(x = Year, y = mean_length), 
+#               method = "gam", col = "black") +
+#   theme(legend.position = "right") +
+#   labs(x = "", y = "Mean length")
